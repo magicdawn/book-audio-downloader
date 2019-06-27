bd4de893a04f62b39fbd894be579a1e603d2fc25
magicdawn <magicdawn@qq.com>
2019-06-27 21:24:33 +0800

    chore: update dot files

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,1 @@
-module.exports = {
-  printWidth: 100,
-  tabWidth: 2,
-  useTabs: false,
-  semi: false,
-  singleQuote: true,
-  quoteProps: 'consistent',
-  jsxSingleQuote: true,
-  trailingComma: 'es5',
-  bracketSpacing: false,
-}
+module.exports = require('@magicdawn/prettier-config')
